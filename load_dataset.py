83addebd22aa60d28e9a0b84e22271cdc3541665
Martin <36505224+OlympicalStoner@users.noreply.github.com>
2021-04-05 14:46:00 +0200

    Get boogied lmao

--- load_dataset.py
+++ load_dataset.py
@@ -8,12 +8,12 @@ from tensorflow.keras.preprocessing import image_dataset_from_directory
 
 
 
-train_dir = "dataset_videoes\\train"
+train_dir = "dataset_videos\\train"
 IMG_SIZE = (224, 224)
 # Batches changes how many images are saved in each data element, if its 20 there will be (number elements in image folder / 20) dataset elements
 # Second element is which calss it belongs to elem[1], can be hard to see if you set shuffle=True
 
-path2 = "dataset_videoes\\train\\01April_2010_Thursday_heute-6694"
+path2 = "dataset_videos\\train\\01April_2010_Thursday_heute-6694"
 path, dirs, files = next(os.walk(train_dir))
 print("Printing directories:")
 print(dirs)
@@ -31,36 +31,34 @@ class_names = train_dataset.class_names
 
 placeholder_lang = []
 
-for i in range(10):
+for i in range(30):
     placeholder_lang.append(i)
 
 placeholder_lang = np.array(placeholder_lang)
 
 merged_dataset = []
+images = []
 count = 0
 for elem in train_dataset:
     image = elem[0].numpy()
+    images.append(image)
     label = int(elem[1].numpy())
+    
     sentence = placeholder_lang[label]
-    #print(sentence)
     combined_tuple = (image, sentence)
-
     merged_dataset.append(combined_tuple)
 
-for (image, label) in merged_dataset:
-    #print(image)
-    print(label)
-
-as_np = np.array(merged_dataset)
+as_np = np.array(merged_dataset, dtype=object)
 print(as_np.shape)
+imgTensor = tf.convert_to_tensor(images)
 
-#tf.convert_to_tensor(merged_dataset)
+#print(type(merged_dataset))
 
 
 #dataset9 = tf.data.Dataset.from_tensor_slices(merged_dataset)
 
 
-dataset = tf.data.Dataset.from_tensor_slices([1, 2, 3, 4, 5, 6, 7, 8])
+dataset = tf.data.Dataset.from_tensor_slices(images)
 #dataset = tf.data.Dataset.from_tensor_slices([[1, 2], [3, 4], [5, 6], [7, 8]])
 dataset = dataset.range(1, 6)  # ==> [ 1, 2, 3, 4, 5 ]
 #lambdafunc = lambda value, labels:  
@@ -93,7 +91,7 @@ dataset = dataset.map(lambda x: tf.py_function(func=upper_case_fn,
 #print(len(train_dataset))
 
 
-# Get number of videoes in folder
+# Get number of videos in folder
 #irr_path, irr_dirs, files = next(os.walk(path2))
 file_count2 = 2 #len(files)
 # Convert folder to tensorflow dataset
